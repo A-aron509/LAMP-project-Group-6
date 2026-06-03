d9f2e68d7d7824d8e33c6ef7b8008b41fa0f59bc
Banks027 <Banks02753@gmail.com>
2026-06-03 12:14:27 -0400

    Add files via upload

---
+++ LAMPAPI/Logout.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../config/helpers.php';
+
+setCORSHeaders();
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$_SESSION = [];
+session_destroy();
+
+respond(200, ['message' => 'Logged out successfully']);
