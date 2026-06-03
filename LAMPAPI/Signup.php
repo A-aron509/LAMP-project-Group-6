c345eeefff05903327ad68572aa34f32ef6f8f43
Banks027 <Banks02753@gmail.com>
2026-06-03 12:27:00 -0400

    Update Signup.php

--- LAMPAPI/Signup.php
+++ LAMPAPI/Signup.php
@@ -1,48 +1,40 @@
- <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
+<?php
+require_once '../config/db.php';
+require_once '../config/helpers.php';
+
+setCORSHeaders();
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    respond(405, ['error' => 'Method not allowed']);
 }
-	$inData = getRequestInfo();
-	
-	$FirstName = $inData["FirstName"]; //Represents the color name that the user wants to add
-	$LastName = $inData["LastName"]; //Represents the user that is adding the color, this is used to make sure the color is added to the correct user
-    $Login = $inData["Login"]; //Represents the user that is adding the color, this is used to make sure the color is added to the correct user
-    $Password = $inData["Password"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,UserName,Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$body = getRequestBody();
+$username = clean($body['username'] ?? '');
+$email    = clean($body['email']    ?? '');
+$password =        $body['password'] ?? '';
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+if (!$username || !$email || !$password) {
+    respond(400, ['error' => 'username, email, and password are required']);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-?>
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    respond(400, ['error' => 'Invalid email address']);
+}
+
+if (strlen($password) < 8) {
+    respond(400, ['error' => 'Password must be at least 8 characters']);
+}
+
+$hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+
+try {
+    $db   = getDB();
+    $stmt = $db->prepare('INSERT INTO users (username, email, password_hash) VALUES (:username, :email, :hash)');
+    $stmt->execute([':username' => $username, ':email' => $email, ':hash' => $hash]);
+    respond(201, ['message' => 'Account created successfully', 'user_id' => (int) $db->lastInsertId()]);
+} catch (PDOException $e) {
+    if ($e->getCode() === '23000') {
+        respond(409, ['error' => 'Username or email already in use']);
+    }
+    respond(500, ['error' => 'Registration failed']);
+}
